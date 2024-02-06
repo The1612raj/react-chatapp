1fc649a5d1cea9ca4ca88060ba2962571eb09c8a
Subhasish Panda <104180552+The1612raj@users.noreply.github.com>
2024-02-07 05:12:29 +0530

    Update firebase.js
    update your firebase js

--- chatApp/src/firebase.js
+++ chatApp/src/firebase.js
@@ -1,12 +1,7 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyATLdrKcyLcTsxdNZoOe0MTGblPp9LguwY",
-  authDomain: "chatappraj-1612.firebaseapp.com",
-  projectId: "chatappraj-1612",
-  storageBucket: "chatappraj-1612.appspot.com",
-  messagingSenderId: "1077833384419",
-  appId: "1:1077833384419:web:ffdad2841fdc864d5960d6"
+  
 };
 
 export const app = initializeApp(firebaseConfig);
